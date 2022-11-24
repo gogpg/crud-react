4b06beabeb5e3c83b57451ea844c648b1b2bebe8
gogpg <giedre.luko@gmail.com>
2022-11-24 21:18:16 +0200

    check box updates

--- src/components/TableBody.jsx
+++ src/components/TableBody.jsx
@@ -4,18 +4,30 @@ import DataContext from "./DataContext";
 
 function TableBody() {
 
-    const {data, dispachData} = useContext(DataContext)
+    const {data, dispachData, setIsCheck} = useContext(DataContext)
+
+    const check = (id, e) => {
+        const c = e.target.checked;
+        dispachData(checkEmplyee_action(id, c))
+
+        if (!c) {
+            setIsCheck(c)
+        }
+    }
+
     return(
         <tbody>
-            {data?.map(e => (
-                 <tr key = {e.id}>
-                    <td><input type='checkbox' onChange={event => dispachData(checkEmplyee_action(e.id, event.target.checked)) } checked={e.check}/></td>
+            {data?.map(e => {
+                return (
+                <tr key = {e.id}>
+                    <td><input type='checkbox' onChange={event => check(e.id, event) } checked={e.check}/></td>
                     <td>{e.name}</td>
                     <td>{e.age}</td>
                     <td>{e.city}</td>
 
                 </tr>
-            ))}
+                )
+})}
         </tbody>
 
     )
